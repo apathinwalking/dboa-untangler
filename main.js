44db77bf13caa5a3d3262dcac6a9c148c361c5e6
Evan Mahone <apathinwalking@gmail.com>
2015-06-03 16:04:05 -0400

    added dboa

---
+++ main.js
@@ -0,0 +1,32 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var config = require('./config.json');
+var knex = require('knex')({client:'pg',connection:config.pg_conn_str});
+var Dboa = require('dboa-js');
+var dboa = new Dboa(knex);
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    // and load the index.html of the app.
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+    // Open the devtools.
+    mainWindow.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
